e9529cacbc140d46851ddbc09b95c3d00c7521d0
anilyaman <anilyaman00@gmail.com>
2020-08-03 23:47:21 +0900

    working version

--- delay_odesolver.py
+++ delay_odesolver.py
@@ -1,9 +1,12 @@
+#!pip install ddeint
+
+
 from scipy.integrate import odeint
 from pylab import *
 import numpy as np
 from ddeint import ddeint
 
-T = 10
+T = 100
 
 def piStar(t):
     return 0.8
@@ -11,52 +14,62 @@ def piStar(t):
 def piHat(t):
     return 0.1
 
-tmp = 0
 
-ICarr = []
-IWarr = []
-Sarr = []
 
 def system(Y, t, d):
     IC, IW, S = Y(t)
     ICd, IWd, Sd = Y(t-d)
 
     fic = (1-epsilon) * piStar(t) + epsilon * piHat(t)
-    fiw = epsilon * piHat(t)
+    fiw = piHat(t)
 
     phi = IC * fic + IW * fiw 
-    fs = phi
+    fs = 0
     
+    ICarr = []
+    IWarr = []
+    Sarr = []
+
     ICarr.append(IC)
     IWarr.append(IW)
     Sarr.append(S)
 
     xbStar = [] 
-    xbStar.append((1-epsilon)*ICarr[0])
     xbHat = [] 
-    xbHat.append(epsilon*ICarr[0]+IWarr[0])
     bS = []
-    bs.append(0)
+    Icounter = [0]
 
-    start=t%d
-    ctr = d
+    bS.append([0,0])
+    xbStar.append((1-epsilon)*ICarr[0])
+    xbHat.append(epsilon*ICarr[0]+IWarr[0])
     
-    if t-d>=t%d:
-        for i in np.arange(start, 5, t):
-            if xbStar[t-1] >= xbHat[t-1]:
-                xbStar.append((1-epsilon)*ICarr[len(ICarr)-1)] + Sarr[len(Sarr)-2] )
-                xbHat.append(epsilon*ICarr[0]+IWarr[0])
-            else:
-                xbStar.append((1-epsilon)*ICarr[0] )
-                xbHat.append(epsilon*ICarr[0]+IWarr[0] + Sarr[len(Sarr)-2])
+    if d!=0:
+        if t-d >= t%d:
+            for i in np.arange(t%d, t, d):
+                Icounter.append(i)
+                if xbStar[len(xbStar)-1] >= xbHat[len(xbHat)-1]:
+                    bS.append([1,0])
+                else:
+                    bS.append([0,1])  
 
-# zero delay:
-#     if (1-epsilon)*ICd >= epsilon*ICd+IWd and t-d>0:
-        # fs = piStar(t)
-        # phi = IC * fic + IW * fiw + S * fs
-    # else:
-        # fs = piHat(t)
-        # phi = IC * fic + IW * fiw + S * fs
+                xbStar.append((1-epsilon)*ICarr[len(ICarr)-1] + Sarr[len(Sarr)-1] * bS[len(bS)-2][0])
+                xbHat.append(epsilon*ICarr[len(ICarr)-1] + IWarr[len(IWarr)-1] + Sarr[len(Sarr)-1] * bS[len(bS)-2][1])
+
+        fs = bS[len(bS)-1][0] * piStar(t) + bS[len(bS)-1][1] * piHat(t)
+        
+        
+
+    phi = IC * fic + IW * fiw + S * fs
+
+
+    # zero delay:
+    if d==0:
+      if (1-epsilon)*ICd >= epsilon*ICd+IWd and t-d>0:
+        fs = piStar(t)
+        phi = IC * fic + IW * fiw + S * fs
+      else:
+        fs = piHat(t)
+        phi = IC * fic + IW * fiw + S * fs
 
     f = np.array([IC * ( fic - phi ) + eta * IW,   \
          IW * ( fiw - phi) - eta * IW,   \
@@ -73,13 +86,11 @@ eta = epsilon
 
 w0 = [IC0, IW0, S0]
 g = lambda t : w0
-# tt = np.linspace(0,T,10)
-tt = np.array([0,1,2,3,4,5,6,7,8,9,10])
-print(tt)
+tt = np.linspace(0,T,5000)
+# tt = np.array([0,1,2,3,4,5,6,7,8,9,10])
 
 fig,ax=subplots(1)
-
-for d in [5]:
+for d in [0, 2, 4, 8]:
     yy = ddeint(system,g,tt,fargs=(d,))
     if d==0:
         ax.plot(tt, yy[:,0],lw=2,label="IC delay = %.01f"%d)
