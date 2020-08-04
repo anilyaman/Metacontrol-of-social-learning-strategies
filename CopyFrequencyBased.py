5d5cf8f4dc8fdf3af48401370b94dc10b6ea4fbf
anilyaman <anilyaman00@gmail.com>
2020-08-04 14:41:58 +0900

    Update CopyFrequencyBased.py

--- CopyFrequencyBased.py
+++ CopyFrequencyBased.py
@@ -1,11 +1,10 @@
-
 from scipy.integrate import odeint
 import numpy as np
 
 
-
 def piStar(t):
     return 0.8
+
 
 def piHat(t):
     return 0.1
@@ -15,59 +14,62 @@ def system(w, t, p):
     IC, IW, S = w
     fic, fiw, fs, eta = p
 
-    fic = (1-epsilon) * piStar(t) + epsilon * piHat(t)
+    fic = (1 - epsilon) * piStar(t) + epsilon * piHat(t)
     fiw = piHat(t)
 
-    phi = IC * fic + IW * fiw 
+    phi = IC * fic + IW * fiw
     fs = 0
-    d=10
+    d = 1
     a = 1
-    
+
     ICarr = []
     IWarr = []
     Sarr = []
-
 
     ICarr.append(IC)
     IWarr.append(IW)
     Sarr.append(S)
 
-    xbStar = [] 
-    xbHat = [] 
+    xbStar = []
+    xbHat = []
     bS = []
-    
-    bS.append([10e-8,10e-8])
-    xbStar.append((1-epsilon)*ICarr[0])
-    xbHat.append(epsilon*ICarr[0]+IWarr[0])
-    if d!=0:
-        if t-d >= t%d:
-            for i in np.arange(t%d, t, d):
-              bS.append([xbStar[len(xbStar)-1],xbHat[len(xbHat)-1]])
-              xbStar.append((1-epsilon)*ICarr[len(ICarr)-1] + Sarr[len(Sarr)-1] * bS[len(bS)-2][0])
-              xbHat.append(epsilon*ICarr[len(ICarr)-1] + IWarr[len(IWarr)-1] + Sarr[len(Sarr)-1] * bS[len(bS)-2][1])
-        
-        
-        xbStarPow = pow(bS[len(bS)-1][0],a)
-        xbHatPow = pow(bS[len(bS)-1][1],a)
+
+    bS.append([10e-8, 10e-8])
+    xbStar.append((1 - epsilon) * ICarr[0])
+    xbHat.append(epsilon * ICarr[0] + IWarr[0])
+    if d != 0:
+        if t - d >= t % d:
+            for i in np.arange(t % d, t, d):
+                bS.append([xbStar[len(xbStar) - 1], xbHat[len(xbHat) - 1]])
+                xbStar.append((1 - epsilon) * ICarr[len(ICarr) - 1] + Sarr[len(Sarr) - 1] * bS[len(bS) - 2][0])
+                xbHat.append(
+                    epsilon * ICarr[len(ICarr) - 1] + IWarr[len(IWarr) - 1] + Sarr[len(Sarr) - 1] * bS[len(bS) - 2][1])
+
+        xbStarPow = pow(bS[len(bS) - 1][0], a)
+        xbHatPow = pow(bS[len(bS) - 1][1], a)
         xbSum = xbStarPow + xbHatPow
         fs = (xbStarPow * piStar(t) + xbHatPow * piHat(t)) / xbSum
-        
-        
-        
+
     phi = IC * fic + IW * fiw + S * fs
 
     # zero delay:
-    #if d==0:
-    #if (1-epsilon)*ICd >= epsilon*ICd+IWd and t-d>0:
+    # if d==0:
+    # if (1-epsilon)*ICd >= epsilon*ICd+IWd and t-d>0:
     #  fs = piStar(t)
     #  phi = IC * fic + IW * fiw + S * fs
-    #else:
+    # else:
     #  fs = piHat(t)
     #  phi = IC * fic + IW * fiw + S * fs
 
-    f = np.array([IC * ( fic - phi ) + eta * IW,   \
-         IW * ( fiw - phi) - eta * IW,   \
-         S  * ( fs  - phi)])
+    #f = np.array([IC * (fic - phi) + eta * IW, \
+    #              IW * (fiw - phi) - eta * IW, \
+    #              S * (fs - phi)])
+
+    Q = [[0.95, 0.025, 0.025], [0.025, 0.95, 0.025], [0.025, 0.025, 0.95]]
+
+    f = np.array([(IC * fic * Q[0][0] + IW * fiw * Q[1][0] + S * fs * Q[2][0]) - IC * phi + eta * IW, \
+                  (IC * fic * Q[0][1] + IW * fiw * Q[1][1] + S * fs * Q[2][1]) - IW * phi - eta * IW, \
+                  (IC * fic * Q[0][1] + IW * fiw * Q[1][1] + S * fs * Q[2][1]) - S * phi])
     return f
 
 
@@ -80,8 +82,8 @@ piBStar = 0.8
 piBHat = 0.1
 eta = epsilon
 
-fic = (1-epsilon) * piBStar + epsilon * piBHat
-fiw = epsilon * piBHat 
+fic = (1 - epsilon) * piBStar + epsilon * piBHat
+fiw = epsilon * piBHat
 fs = fic
 
 p = [fic, fiw, fs, eta]
@@ -98,34 +100,33 @@ t = [stoptime * float(i) / (numpoints - 1) for i in range(numpoints)]
 wsol = odeint(system, w0, t, args=(p,),
               atol=abserr, rtol=relerr)
 
-T=[]
-IC=[]
-IW=[]
-S=[]
-I=[]
+T = []
+IC = []
+IW = []
+S = []
+I = []
 for t1, w1 in zip(t, wsol):
-  T.append(t1)
-  IC.append(w1[0])
-  IW.append(w1[1])
-  S.append(w1[2])
-  I.append(w1[0] + w1[1])
+    T.append(t1)
+    IC.append(w1[0])
+    IW.append(w1[1])
+    S.append(w1[2])
+    I.append(w1[0] + w1[1])
 
-
-#!pip install pylab
-from pylab import figure, plot, xlabel, grid, legend, title
+# !pip install pylab
+import pylab as plt
+from pylab import figure, xlabel, grid, title
 from matplotlib.font_manager import FontProperties
-
-
 
 figure(1, figsize=(6, 4.5))
 
 xlabel('t')
 lw = 2
 
-plot(t, IC, 'b', linewidth=lw)
-plot(t, IW, 'g', linewidth=lw)
-plot(t, S, 'k', linewidth=lw)
-plot(t, I, 'y', linewidth=lw)
+plt.plot(t, IC, 'b', linewidth=lw)
+plt.plot(t, IW, 'g', linewidth=lw)
+plt.plot(t, S, 'k', linewidth=lw)
+plt.plot(t, I, 'y', linewidth=lw)
 
-legend((r'$IC$', r'$IW$', r'$S$', r'$I (=IC+IW)$'), prop=FontProperties(size=16))
+plt.legend((r'$IC$', r'$IW$', r'$S$', r'$I (=IC+IW)$'), prop=FontProperties(size=16))
 # title('test')
+plt.show()
