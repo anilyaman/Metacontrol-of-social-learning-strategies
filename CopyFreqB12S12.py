4d3a12f2a5013922d81978d9ac4d2d9d1e0f896d
anilyaman <anilyaman00@gmail.com>
2020-08-06 20:57:45 +0900

    Update CopyFreqB12S12.py

--- CopyFreqB12S12.py
+++ CopyFreqB12S12.py
@@ -6,7 +6,7 @@ import math
 # ODE solver parameters
 abserr = 1.0e-8
 relerr = 1.0e-6
-numpoints = 1000
+numpoints = 1500
 stoptime = 300.0
 
 tt = [stoptime * float(i) / (numpoints - 1) for i in range(numpoints)]
@@ -50,11 +50,10 @@ def system(w, t, p):
     else:
         fB1 = (1 - epsilon) * piB1(t) + epsilon * piB2(t)
         fB2 = (1 - epsilon) * piB2(t) + epsilon * piB1(t)
-    fS1 = 0
-    fS2 = 0
+
 
     d = 10
-    a = 2
+    a = 1.5
 
     tempArr = np.abs(np.array(tt) - t)
     tempArr = tempArr.tolist()
@@ -75,62 +74,43 @@ def system(w, t, p):
     xB2 = []
     xSb1 = []
     xSb2 = []
-    #xB1.append((1 - epsilon) * B1arr[0])
-    #xB2.append((1-epsilon) * B2arr[0] + epsilon * B1arr[0])
-    #xB1norm = pow(xB1[len(xB1) - 1], a)
-    #xB2norm = pow(xB2[len(xB2) - 1], a)
-    #xSb1.append(xB1norm / (xB1norm + xB2norm))
-    #xSb2.append(xB2norm / (xB1norm + xB2norm))
 
     xSb1.append(0)
     xSb2.append(0)
     for i in np.arange(inx%d, inx, d):
         #if Tarr[i] < stoptime/2:
-        xB1.append((1-epsilon) * B1arr[i] + epsilon * B2arr[i] + S1arr[i] * xSb1[len(xSb1) - 1] )
-        xB2.append((1-epsilon) * B2arr[i] + epsilon * B1arr[i] + S2arr[i] * xSb2[len(xSb2) - 1])
-        if(xB1[len(xB1) - 1] != 0):
-            xB1norm = pow(xB1[len(xB1) - 1], a)
-        if (xB2[len(xB2) - 1] != 0):
-            xB2norm = pow(xB2[len(xB2) - 1], a)
+        appInx = i
+        if(math.isnan(B1arr[i])) or B1arr[i] == 0:
+            for j in range(i-1,0,-1):
+                if(not  math.isnan(B1arr[j]) and  B1arr[j]!=0):
+                    appInx = j
+                    break
+        xB1.append((1-epsilon) * B1arr[appInx] + epsilon * B2arr[appInx] + S1arr[appInx] * xSb1[len(xSb1) - 1])
+        xB2.append((1-epsilon) * B2arr[appInx] + epsilon * B1arr[appInx] + S2arr[appInx] * xSb2[len(xSb2) - 1])
+        xB1norm = pow(xB1[len(xB1) - 1], a)
+        xB2norm = pow(xB2[len(xB2) - 1], a)
         xSb1.append((xB1norm / (xB1norm + xB2norm)))
         xSb2.append((xB2norm / (xB1norm + xB2norm)))
-        #else:
-        #    xB1.append((1-epsilon) * B1arr[i] + epsilon * B2arr[i] + S1arr[i] * xSb1[len(xSb1) - 1] )
-        #    xB2.append((1-epsilon) * B2arr[i] + epsilon * B1arr[i] + S2arr[i] * xSb2[len(xSb2) - 1])
-        #    xB1norm = pow(xB1[len(xB1) - 1], a)
-        #    xB2norm = pow(xB2[len(xB2) - 1], a)
-        #    xSb1.append((xB1norm / (xB1norm + xB2norm)))
-        #    xSb2.append((xB2norm / (xB1norm + xB2norm)))
 
 
-        fS1 = xSb1[len(xSb1)-1] * piB1(t) + xSb2[len(xSb2)-1] * piB2(t)
-        fS1arr[inx] = fS1
-        evaluated[inx] = 1
-        fS2 = 0#xSb2[len(xSb2)-1] * piB2(t)
-        fS2arr[inx] = fS2
+    fS1 = xSb1[len(xSb1)-1] * piB1(t)
+    fS1arr[inx] = fS1
+    fS2 = xSb2[len(xSb2)-1] * piB2(t)
+    fS2arr[inx] = fS2
+    evaluated[inx] = 1
 
 
 
-    phi = B1 * fB1 + B2 * fB2 + S1 * fS1 + S2 * fS2
+    phi = B1 * fB1 + B2 * fB2 + S1 * fS1 + + S2 * fS2
     phiarr[inx] = phi
 
     Q = [[0.998, 0, 0.001, 0.001], [0, 0.998, 0.001, 0.001], [0.0005, 0.0005, 0.999, 0], [0.0005, 0.0005, 0, 0.999]]
 
 
-    if t  < stoptime / 2:
-#        f = np.array([(B1 * fB1 * Q[0][0] + B2 * fB2 * Q[1][0] + S1 * fS1 * Q[2][0] + S2 * fS2 * Q[3][0]) - B1 * phi + eta * B2, \
-#                      (B1 * fB1 * Q[0][1] + B2 * fB2 * Q[1][1] + S1 * fS1 * Q[2][1] + S2 * fS2 * Q[3][1]) - B2 * phi - eta * B2, \
-        f = np.array([(B1 * fB1 * Q[0][0] + B2 * fB2 * Q[1][0] + S1 * fS1 * Q[2][0] + S2 * fS2 * Q[3][0]) - B1 * phi, \
-                      (B1 * fB1 * Q[0][1] + B2 * fB2 * Q[1][1] + S1 * fS1 * Q[2][1] + S2 * fS2 * Q[3][1]) - B2 * phi,\
-                      (B1 * fB1 * Q[0][2] + B2 * fB2 * Q[1][2] + S1 * fS1 * Q[2][2] + S2 * fS2 * Q[3][2]) - S1 * phi, \
-                      (B1 * fB1 * Q[0][2] + B2 * fB2 * Q[1][2] + S1 * fS1 * Q[2][2] + S2 * fS2 * Q[3][3]) - S2 * phi])
-    else:
-#        f = np.array([(B1 * fB1 * Q[0][0] + B2 * fB2 * Q[1][0] + S1 * fS1 * Q[2][0] + S2 * fS2 * Q[3][0]) - B1 * phi - eta * B1, \
-#                      (B1 * fB1 * Q[0][1] + B2 * fB2 * Q[1][1] + S1 * fS1 * Q[2][1] + S2 * fS2 * Q[3][1]) - B2 * phi + eta * B1, \
-        f = np.array([(B1 * fB1 * Q[0][0] + B2 * fB2 * Q[1][0] + S1 * fS1 * Q[2][0] + S2 * fS2 * Q[3][0]) - B1 * phi, \
-                      (B1 * fB1 * Q[0][1] + B2 * fB2 * Q[1][1] + S1 * fS1 * Q[2][1] + S2 * fS2 * Q[3][1]) - B2 * phi, \
-                      (B1 * fB1 * Q[0][2] + B2 * fB2 * Q[1][2] + S1 * fS1 * Q[2][2] + S2 * fS2 * Q[3][2]) - S1 * phi, \
-                      (B1 * fB1 * Q[0][2] + B2 * fB2 * Q[1][2] + S1 * fS1 * Q[2][2] + S2 * fS2 * Q[3][3]) - S2 * phi])
+    f = np.array([(B1 * fB1 * Q[0][0] + B2 * fB2 * Q[1][0] + S1 * fS1 * Q[2][0] + S2 * fS2 * Q[3][0]) - B1 * phi,
+                  (B1 * fB1 * Q[0][1] + B2 * fB2 * Q[1][1] + S1 * fS1 * Q[2][1] + S2 * fS2 * Q[3][1]  ) - B2 * phi,
+                  (B1 * fB1 * Q[0][2] + B2 * fB2 * Q[1][2] + S1 * fS1 * Q[2][2] + S2 * fS2 * Q[3][2] ) - S1 * phi,
+                 (B1 * fB1 * Q[0][3] + B2 * fB2 * Q[1][3] + S1 * fS1 * Q[2][3] + S2 * fS2 * Q[3][3]) - S2*phi])
 
     return f
 
@@ -202,6 +182,6 @@ plot(tt,fS1arr, 'k', linewidth=lw)
 plot(tt,fS2arr, 'm', linewidth=lw)
 plot(tt,phiarr, 'r', linewidth=lw)
 
-legend((r'$fB1arr$', r'$fB2arr$', r'$fS1arr$', r'$fS2arr$', r'$phiarr$'), prop=FontProperties(size=16))
+legend((r'$fB1arr$', r'$fB2arr$', r'$fS1arr$',r'$fS2arr$', r'$phiarr$'), prop=FontProperties(size=16))
 # title('test')
 show()
