8bd05ad364c1853e310d8946f846e28c420df421
anilyaman <anilyaman00@gmail.com>
2021-10-05 14:31:34 +0900

    Update runEA.m

--- EvolutionaryAlgorithm/runEA.m
+++ EvolutionaryAlgorithm/runEA.m
@@ -18,14 +18,14 @@ settings.std1 = [0.05 0.05  0.05];%std of the reward distributions for the first
 settings.mu2 =  [0.5 0.5  0.7];%mean of the reward distributions for the second arm in each period
 settings.std2 = [0.05 0.05  0.5];%std of the reward distributions for the second arm in each period
 settings.randReversal = [1 1 1]; %whether to apply reward reversal that is swithcing the means and stds of the arms for each period
-nRun = 28; %the number of runs
+nRun = 4*28; %the number of runs
 
 
 settings.nArm = 2; %the number of arms
 
 
 
-%type of strategy: individual learning 
+%type of strategy: individual learning
 settings.strategy = 'individual';
 nIL = zeros(nRun,settings.T);
 nSL = zeros(nRun,settings.T);
@@ -33,7 +33,7 @@ phi = zeros(nRun,settings.T);
 conformityCoef = zeros(nRun,settings.T);
 varCoef = zeros(nRun,settings.T);
 
-for i=1:nRun
+parfor i=1:nRun
     res = onlyIndividualLearning(settings);
     nIL(i,:) = res.nIL;
     nSL(i,:) = res.nSL;
@@ -49,13 +49,13 @@ results{1}.conformityCoef = conformityCoef;
 results{1}.varCoef = varCoef;
 
 
-%type of strategy: conformist social learning  
+%type of strategy: conformist social learning
 counter = 2;
 settings.strategy = 'conformity';
 runAlgorithm();
 
 
-%type of strategy: success-based social learning 
+%type of strategy: success-based social learning
 counter = 3;
 settings.strategy = 'success-based';
 runAlgorithm();
@@ -74,7 +74,7 @@ save results results
         conformityCoef = zeros(nRun,settings.T);
         varCoef = zeros(nRun,settings.T);
         
-        for i=1:nRun
+        parfor i=1:nRun
             res = EvolutionaryAlgorithm(settings);
             nIL(i,:) = res.nIL;
             nSL(i,:) = res.nSL;
@@ -91,69 +91,7 @@ save results results
         results{counter}.settings = settings;
         results{counter}.aveBehaviorDist = behaviorDist./nRun;
         
-        return;
-        %generating the plots
-        figure;
-        hold on
-        tt = 1:settings.T;
-        plot(tt,median(results{counter}.nIL),'Color', 'k', 'Linewidth', 2)
         
-        
-        med = median(results{counter}.nIL);
-        stdX = std(results{counter}.nIL);
-        x = 1:numel(med);
-        x2 = [x, fliplr(x)];
-        std_dev = stdX;
-        curve1 = med + 1.*std_dev;
-        curve2 = med - 1.*std_dev;
-        inBetween = [curve1, fliplr(curve2)];
-        f = fill(x2, inBetween, 'k', 'FaceAlpha',0.1, 'EdgeColor','none');
-        set(get(get(f(1),'Annotation'),'LegendInformation'),'IconDisplayStyle','off');
-        
-        plot(tt,median(results{counter}.nSL),'Color', 'r', 'Linewidth', 2)
-        
-        med = median(results{counter}.nSL);
-        stdX = std(results{counter}.nSL);
-        x = 1:numel(med);
-        x2 = [x, fliplr(x)];
-        std_dev = stdX;
-        curve1 = med + 1.*std_dev;
-        curve2 = med - 1.*std_dev;
-        inBetween = [curve1, fliplr(curve2)];
-        f = fill(x2, inBetween, 'r', 'FaceAlpha',0.1, 'EdgeColor','none');
-        set(get(get(f(1),'Annotation'),'LegendInformation'),'IconDisplayStyle','off');
-        
-        if(settings.nArm == 2)
-            plot(tt,median(results{counter}.B1),'--','Color', [0.07,0.62,1.00], 'Linewidth', 2)
-            plot(tt,median(results{counter}.B2),'--','Color', 'g', 'Linewidth', 2)
-        end
-        xlabel('Generations (t)')
-        ylabel('Ratio')
-        set(gca,'FontSize',15);
-        ylim([0,1.1])
-
-        yyaxis right
-        ylabel('\phi(t)')
-        set(gca,'ycolor','m');
-        ylim([0,1.1])
-
-        plot(tt, mean(results{counter}.phi), 'Color', 'm','Linewidth', 2)
-        if(settings.nArm == 2)
-            legend({'IL','SL','B_1','B_2', '\phi(t)'}, ...
-                'Position',[0.22 0.85 0.64 0.08], ...
-                'FontSize',13,...
-                'NumColumns',5,...
-                'color','none');
-            legend boxoff
-        else
-%             plot(tt, mean(results{1}.phi), 'Color', 'b','Linewidth', 2)
-            legend({'IL','SL', '\phi(t)','\phi(t)-IL'}, ...
-                'Position',[0.22 0.85 0.64 0.08], ...
-                'FontSize',13,...
-                'NumColumns',5,...
-                'color','none');
-            legend boxoff
-        end
     end
 
 
